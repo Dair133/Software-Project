e7daa1bff4b25301a03f8fcae65e3b6e2317c7bc
Dair133 <114807176+Dair133@users.noreply.github.com>
2023-03-01 18:58:56 +0000

    Foundation for dynamic player tracking done,still needs a lot of work and testing but good prgoress has been made

--- Pages/Navigation.vue
+++ Pages/Navigation.vue
@@ -10,16 +10,20 @@
 <router-link to ="/test" class =btn-primary>Node Test Page</router-link><br>
 <router-link to ="/Realtime" class =btn-primary>Realtime test</router-link><br>
 <router-link to ="/ideTest" class =btn-primary>New IDE test</router-link><br>
-<router-link to ="/test" style="visibility:hidden" class =btn-primary>Node Test Page</router-link><br>
-<router-link to ="/test" style="visibility:hidden" class =btn-primary>Node Test Page</router-link><br>
-<router-link to ="/test" style="visibility:hidden" class =btn-primary>Node Test Page</router-link><br>
-<router-link to ="/test" style="visibility:hidden" class =btn-primary>Node Test Page</router-link><br>
-<router-link to ="/test" style="visibility:hidden" class =btn-primary>Node Test Page</router-link><br>
-<router-link to ="/test" style="visibility:hidden" class =btn-primary>Node Test Page</router-link><br>
-<router-link to ="/test" style="visibility:hidden" class =btn-primary>Node Test Page</router-link><br>
-<router-link to ="/test" style="visibility:hidden" class =btn-primary>Node Test Page</router-link><br>
-<router-link to ="/test" style="visibility:hidden" class =btn-primary>Node Test Page</router-link><br>
+<div id="name">z</div>
+<div id="numPlayers">z</div>
+<div id="uid">z</div><br><br>
+<div load id="listOfNames">z<span id="location"></span></div>
 
+<router-link to ="/test" style="visibility:hidden" class =btn-primary>Node Test Page</router-link><br>
+<router-link to ="/test" style="visibility:hidden" class =btn-primary>Node Test Page</router-link><br>
+<router-link to ="/test" style="visibility:hidden" class =btn-primary>Node Test Page</router-link><br>
+<router-link to ="/test" style="visibility:hidden" class =btn-primary>Node Test Page</router-link><br>
+<router-link to ="/test" style="visibility:hidden" class =btn-primary>Node Test Page</router-link><br>
+<router-link to ="/test" style="visibility:hidden" class =btn-primary>Node Test Page</router-link><br>
+<router-link to ="/test" style="visibility:hidden" class =btn-primary>Node Test Page</router-link><br>
+<router-link to ="/test" style="visibility:hidden" class =btn-primary>Node Test Page</router-link><br>
+<router-link to ="/test" style="visibility:hidden" class =btn-primary>Node Test Page</router-link><br>
 
 </body>
     </template>
@@ -59,3 +63,137 @@ color:white;
   background: radial-gradient(circle at 6.6% 12%, rgb(64, 0, 126) 20.8%, rgb(0, 255, 160) 100.2%);
 }
         </style>
+        <script>
+          import { getDatabase, ref, set,onValue,get, onDisconnect,onChildRemoved,child,onChildAdded } from "firebase/database";
+   import test from '../main';
+   import loader from "@monaco-editor/loader";
+import { listen } from "vscode-ws-jsonrpc";
+import  firebase  from 'firebase/compat/app';
+import {  push,serverTimestamp,update } from "firebase/database";
+const db = getDatabase();
+const currentUserRef = ref(db,'Online');
+const listOfPlayers = ref(db,'ListOfPLayers/names');
+    
+  
+
+
+//maybe get rid of numOfUsers child as it may not be necessary
+const connectedRef = ref(db,".info/connected");
+onValue(connectedRef,(snap) =>{
+  console.log(snap)
+  if(snap.val() ==true){
+    const realTime = test.methods.intaliseRealTimeDatabase();
+    // const con = push(child(currentUserRef,document.getElementById("name").innerHTML),'inside child');
+  const Parent= child(currentUserRef,document.getElementById("name").innerHTML)
+    const creatingName= (child (Parent,'Name',));
+    const  creatingUid = (child (Parent,'Uid',));
+    const  creatingCurrentPage = (child (Parent,'CurrentPage',));
+    const settingUid = set(creatingUid,document.getElementById("uid").innerHTML)
+    const settingName= set(creatingName,document.getElementById("name").innerHTML)//working kinda at least
+    console.log(window.location.href)
+   // const settingCurrentPage = set(creatingCurrentPage,window.location.href)
+     const con2 = push(listOfPlayers,document.getElementById("name").innerHTML)
+     onDisconnect(con2).remove()
+     onDisconnect(Parent).remove()
+    // onDisconnect(con).remove();
+     //set(con,true)
+    }
+    if(snap.val() == false){
+      // const realTime = test.methods.intaliseRealTimeDatabase();
+      // set(ref(realTime,'user33/'),{
+      //   numberOfPlayers:10,
+      //   status:"offline"
+      // })
+
+    }
+    
+    onValue(listOfPlayers,(snapshot) =>{
+      var holder =0;
+snapshot.forEach(function (snapshot){
+holder+=1;
+document.getElementById("numPlayers").innerHTML = holder;
+
+})
+
+}).bind(this)
+
+// onValue(listOfPlayers,(snapshot) =>{
+//   document.getElementById("listOfNames").innerHTML ="";
+// var name ="";
+// snapshot.forEach(function (snapshot){//goes through list /ListOfPLayers in realtime database and adds them to the html tag 
+//   console.log("name snapshot "+snapshot.val())
+//   name = snapshot.val()
+//   document.getElementById("listOfNames").innerHTML +="<br>"+name+;
+// })
+
+// })
+
+
+
+
+   setTimeout(() =>{
+    const OnlineRef = ref(db,'Online/');
+onValue(OnlineRef,(snapshot) =>{
+  document.getElementById("listOfNames").innerHTML ="";
+  console.log(snapshot.val())
+  var snapObject = snapshot.val()
+  console.log("current final page is"+snapObject.CurrentPage)
+
+
+snapshot.forEach(function (snapshot){//goes through list /ListOfPLayers in realtime database and adds them to the html tag 
+var snapObject = snapshot.val()
+var name = snapObject.Name
+var url  =  snapObject.CurrentPage
+document.getElementById("listOfNames").innerHTML += "<br>"+name+" "+url+"<br>"
+})
+
+})
+    document.getElementById("numPlayers").style.visibility = "visible"
+    //const con2 = push(listOfPlayers,document.getElementById("name").innerHTML)
+},1000)
+}).bind(this)
+
+export default {
+
+mounted(){
+  firebase.auth().onAuthStateChanged(function(user) {//gets 'user' onbject when it has been intialized 
+  if (user) {
+ 
+    console.log(user.uid)
+    console.log(user.displayName)
+    
+    document.getElementById("name").innerHTML = user.displayName
+    document.getElementById("uid").innerHTML = user.uid
+    //document.getElementById("displayName").innerHTML = user.displayName//displays
+    //document.getElementById("email").innerHTML = user.email   
+    update(ref(db,'Online/'+user.displayName),{
+        CurrentPage: window.location.href
+      })
+
+    return user.displayName
+  } 
+  else {
+    console.log("Error signing in")
+  }
+  }).bind(this)
+
+},
+
+
+  methods: {
+
+
+
+
+  }
+}
+
+
+//   console.log("hopefully not now undefined is "+document.getElementById("name").innerHTML)
+//   const db = getDatabase();
+//   console.log("after database")
+// const reference = ref(db,'Online/'+document.getElementById("name").innerHTML)
+// console.log("after referecnce")
+// var location = window.location.href
+
+      </script>
--- Pages/Realtime.vue
+++ Pages/Realtime.vue
@@ -3,10 +3,11 @@
     
     </head>
     <body>
-      
- 
-    
-        //html tags go here
+    <div id="dataStorage">Example database storage</div>
+    <div id="uid">Example database storage</div>    
+    <div id="name">Example database storage</div>  
+    <div id="numPlayers" style="visibility:hidden;">Example database storage</div>  
+    <div id="listOfNames"></div>
         
         <p id =p1></p>
         <button @click="testingRealTime()">Testing real time</button>
@@ -16,49 +17,130 @@
     </template>
     
     <script>
-    import { getDatabase, ref, set,onValue } from "firebase/database";
+    import { getDatabase, ref, set,onValue,get, onDisconnect,onChildRemoved,child,onChildAdded,update } from "firebase/database";
    import test from '../main';
+   import loader from "@monaco-editor/loader";
+import { listen } from "vscode-ws-jsonrpc";
+import  firebase  from 'firebase/compat/app';
+import {  push,serverTimestamp } from "firebase/database";
+
+    import {  getDownloadURL } from "firebase/storage";
 
 
-const db = getDatabase();
-const starCountRef0 = ref(db, 'users/');
-console.log("datbase functionw roking")
-onValue(starCountRef0, (snapshot) => {
-  var data = snapshot.val();
-  document.getElementById("realTime0").innerHTML = data.username;
-});
 
-const starCountRef1 = ref(db, 'users2/');
-onValue(starCountRef1, (snapshot) => {
-  var data = snapshot.val();
-  document.getElementById("realTime1").innerHTML = data.username;
-});
+// setTimeout(() =>{
+// console.log("inside html tag is"+document.getElementById("name").innerHTML)
 
-//   var vm = test.methods.intialiseVue(
-//     {
-// el:'example',
-// data:{
-//     name:'Vue.js'
-// },
+
+// update(ref(db,'Online/'+document.getElementById("name").innerHTML),{
+//       CurrentPage: window.location.href
+//     })
+// },3000)
+// const starCountRef0 = ref(db, 'users/');
+// console.log("datbase functionw roking")
+// onValue(starCountRef0, (snapshot) => {
+//   var data = snapshot.val();
+//   document.getElementById("realTime0").innerHTML = data.editorData;
+// });
+
+//  const starCountRef1 = ref(db, 'users2/');
+// onValue(starCountRef1, (snapshot) => {
+//   var data = snapshot.val();
+//   document.getElementById("realTime1").innerHTML = data.editorData;
+// });
+
+;
+
+// Set up a listener for when the user disconnects
+
+//http://localhost:5173/Realtime
+//the following code detects if a user is online or offline
+
+
+ 
 export default{
+  data(){
+    return{
+      points:null,
+      numPlayers:""
+    }
+  },
+  mounted(){
+    const db = getDatabase();
+firebase.auth().onAuthStateChanged(function(user) {//gets 'user' onbject when it has been intialized 
+  if (user) {
+ 
+    console.log(user.uid)
+    console.log(user.displayName)
+    
+    document.getElementById("name").innerHTML = user.displayName
+    document.getElementById("uid").innerHTML = user.uid
+    //document.getElementById("displayName").innerHTML = user.displayName//displays
+    //document.getElementById("email").innerHTML = user.email   
+    
+
+    update(ref(db,'Online/'+user.displayName),{
+      CurrentPage: window.location.href
+    })
+   
+    return user.displayName
+  } 
+  else {
+    console.log("Error signing in")
+  }
+  }).bind(this)
+  },
 methods:{
     testingRealTime:function(){
      var text = document.getElementById("realTime0").value
       const realTime = test.methods.intaliseRealTimeDatabase();//intialises relatime database with imported function from main
       set(ref(realTime,'users/'),{
-        username: text,
-        email:'alasdairjball@gmail.com'
+        editorData: text,
+        
       })
       
+},
+awaitPlayerData: async function(){
+var promise = await this.getCorrectData()
+console.log("inside await player dayta"+promise.numberOfPlayers)
+
 },
 testingRealTime2:function(){
   var text = document.getElementById("realTime1").value
       const realTime = test.methods.intaliseRealTimeDatabase();//intialises relatime database with imported function from main
       set(ref(realTime,'users2/'),{
-        username: text,
-        email:'alasdairjball@gmail.com'
+        editorData: text,
+        
       })
-      }
+      },
+      getCorrectData:async function(){
+        const db = test.methods.intaliseRealTimeDatabase();
+ var user;
+  const snapshot = await get(ref(db, '/globalPageData'))
+  user = snapshot.val();
+  console.log(user)
+  return user
+
+
+      },
+      auth:function(){
+    firebase.auth().onAuthStateChanged(function(user) {//gets 'user' onbject when it has been intialized 
+  if (user) {
+ 
+    console.log(user.uid)
+    console.log(user.displayName)
+    
+    document.getElementById("name").innerHTML = user.displayName
+    //document.getElementById("displayName").innerHTML = user.displayName//displays
+    //document.getElementById("email").innerHTML = user.email   
+    return user.displayName
+  } 
+  else {
+    console.log("Error signing in")
+  }
+  }).bind(this)
+},
+     
 }
 }
     
