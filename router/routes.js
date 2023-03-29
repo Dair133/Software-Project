c93563124c01b25956be97c64d0db2d01b92a108
DLiu02 <122496641+DLiu02@users.noreply.github.com>
2023-03-29 13:13:17 +0100

    Add files via upload

--- router/routes.js
+++ router/routes.js
@@ -1,3 +1,22 @@
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+/*import app from "/firebase"*/
+function isAuth(to, from, next){
+    console.log("Checking auth");
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            console.log(user);
+// User is signed in so continue to desired page
+            return next();
+// ...
+        } else {
+// User is signed out
+// Send them back to the home page or maybe the login page
+            return next({path : '/'});
+        }
+    });
+}
+
 function loadPage (component) {
     // '@' is aliased to src/components
     return () => import(/* webpackChunkName: "[request]" */
@@ -12,9 +31,12 @@ function loadPage (component) {
     {path:'/homepage',component:loadPage('Homepage')},
     {path:'/screenshots',component:loadPage('Screenshots')},
     {path:'/test',component:loadPage('Test')},
-    {path:'/play',component:loadPage('Play')},
+    {path:'/play',component:loadPage('Play'), beforeEnter: isAuth },
     {path:'/random',component:loadPage('Random')},
     {path:'/realtime',component:loadPage('Realtime')},
-        {path:'/settings',component:loadPage('Settings')},
-        {path:'/stats',component:loadPage('Statistics')},
+        {path:'/settings',component:loadPage('Settings'), beforeEnter: isAuth},
+        {path:'/stats',component:loadPage('Statistics'), beforeEnter: isAuth},
+        {path:'/idetest',component:loadPage('IdeTest'), beforeEnter: isAuth},
+        {path:'/help',component:loadPage('Help')},
+        {path:'/aboutus',component:loadPage('About')},
     ]
